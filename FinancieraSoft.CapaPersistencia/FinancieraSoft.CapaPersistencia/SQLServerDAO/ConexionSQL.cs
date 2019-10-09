6aec432578d487c2ae4ea5c295dfce961d85fd48
Joel <joel.penasuarez@gmail.com>
2019-10-09 09:08:52 -0500

    Agregando la conexionSQL

---
+++ FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/ConexionSQL.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Data.SqlClient;
+
+namespace FinancieraSoft.CapaPersistencia.SQLServerDAO
+{
+    class ConexionSQL
+    {
+        public SqlConnection EstablecerConexion()
+        {
+            string cadenaDeConexion;
+            cadenaDeConexion = "Data Source=(local);" +
+                "Initial Catalog = FiancieraSoft;" +
+                "Integrated Security=True";
+
+            SqlConnection conexion = new SqlConnection(cadenaDeConexion);
+
+            return conexion;
+        }
+    }
+}
