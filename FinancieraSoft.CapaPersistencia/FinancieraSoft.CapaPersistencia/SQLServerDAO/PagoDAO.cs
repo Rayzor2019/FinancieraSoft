0e5a0382cf600cc63a61c2648f247d884c8d86b5
Rayzor <joel.penasuarez@gmail.com>
2019-10-15 14:45:47 -0500

    Avance Capa Persistencia

--- FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PagoDAO.cs
+++ FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PagoDAO.cs
@@ -9,5 +9,52 @@ namespace FinancieraSoft.CapaPersistencia.SQLServerDAO
 {
     class PagoDAO
     {
+
+        private GestorSQL gestorSQL;
+
+        public PagoDAO(GestorSQL gestorSQL)
+        {
+            this.gestorSQL = gestorSQL;
+        }
+
+        public void guardar(Pago pago)
+        {
+            // CREANDO LAS SENTENCIAS SQL
+            string insertarPago1SQL, insertarPago2SQL
+;
+
+            insertarPago1SQL = "SP_Pago1";
+
+            insertarPago2SQL = "SP_Pago2";
+
+            try
+            {
+                SqlCommand comando;
+                Pago c;
+                // GUARDANDO EL OBJETO Pago
+                if (Pago.couta != null)
+                {
+                    comando = gestorSQL.obtenerComandoSQL(insertarPago1SQL);
+                    comando.Parameters.AddWithValue("@coutaID", c.couta.CoutaID);
+                }
+                else
+                {
+                    comando = gestorSQL.obtenerComandoSQL(insertarPago2SQL);
+                }
+                comando.Parameters.AddWithValue("@idpago", c.PagoID);
+                comando.Parameters.AddWithValue("@diasmora", c.DiasMora);
+                comando.Parameters.AddWithValue("@montorecibido", c.MontoRecibido);
+                comando.Parameters.AddWithValue("@montototal", c.MontoTotal);
+                comando.Parameters.AddWithValue("@mora", c.Mora);
+                comando.Parameters.AddWithValue("@vuelto", c.Vuelto);
+                comando.ExecuteNonQuery();
+
+
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Ocurrio un problema al intentar guardar.", err);
+            }
+        }
     }
 }
