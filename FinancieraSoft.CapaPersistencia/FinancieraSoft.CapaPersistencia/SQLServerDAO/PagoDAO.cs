e901f7324b1451e8ef2715a50b5cca75029d4eea
Rayzor <joel.penasuarez@gmail.com>
2019-10-15 23:13:14 -0500

    Avance Capa Persistencia
    Se actualizo ConsultarPagoDAO V1

--- FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PagoDAO.cs
+++ FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PagoDAO.cs
@@ -4,6 +4,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Data.SqlClient;
+using FinancieraSoft.CapaDominio.Entidades;
 
 namespace FinancieraSoft.CapaPersistencia.SQLServerDAO
 {
@@ -55,6 +56,67 @@ namespace FinancieraSoft.CapaPersistencia.SQLServerDAO
             {
                 throw new Exception("Ocurrio un problema al intentar guardar.", err);
             }
+
         }
+        public List<Pago> buscar(string pagoID)
+        {
+            List<Pago> listaDePagos = new List<Pago>();
+            Pago pago;
+            string consultaSQL = "select * from Pago where pagoID like '%" + pagoID + "%' order by pagoID";
+            try
+            {
+                SqlDataReader resultadoSQL = gestorSQL.ejecutarConsulta(consultaSQL);
+                while (resultadoSQL.Read())
+                {
+                    pago = obtenerPago(resultadoSQL);
+
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            return listaDePagos;
+        }
+
+        public Pago ConsultarPorfecha(string fecha)
+        {
+            Pago pago;
+            string consultaSQL = "select * from Couta where fecha = '" + fecha + "'";
+            try
+            {
+                SqlDataReader resultadoSQL = gestorSQL.ejecutarConsulta(consultaSQL);
+                if (resultadoSQL.Read())
+                {
+                    pago = obtenerPago(resultadoSQL);
+                }
+                else
+                {
+                    throw new Exception("No existe el pago.");
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            return pago;
+        }
+
+        private Pago obtenerPago(SqlDataReader resultadoSQL)
+        {
+            Pago pago = new Pago();
+            pago.PagoID = resultadoSQL.GetString(0);
+            pago.fecha = resultadoSQL.GetDateTime(1);
+            pago.DiasMora = resultadoSQL.GetInt32(2);
+            pago.MontoRecibido = Double.Parse(resultadoSQL.GetDecimal(3).ToString());
+            pago.MontoTotal = resultadoSQL.GetInt32(4);
+            pago.Mora = resultadoSQL.GetDouble(5);
+
+            return pago;
+        }
+
     }
 }
+    
+    
+
