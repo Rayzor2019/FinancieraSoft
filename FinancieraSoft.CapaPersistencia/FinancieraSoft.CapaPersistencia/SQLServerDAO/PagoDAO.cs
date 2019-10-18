23d67d19ddd5fe2f2287ebe7e08f3573937a2131
Christian <yeicosammedina@gmail.com>
2019-10-18 00:34:02 -0500

    pago y prestamo DAO V1

--- FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PagoDAO.cs
+++ FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PagoDAO.cs
@@ -9,5 +9,114 @@ namespace FinancieraSoft.CapaPersistencia.SQLServerDAO
 {
     class PagoDAO
     {
+        private GestorSQL gestorSQL;
+
+        public PagoDAO(GestorSQL gestorSQL)
+        {
+            this.gestorSQL = gestorSQL;
+        }
+
+        public void guardar(Pago pago)
+        {
+            // CREANDO LAS SENTENCIAS SQL
+            string insertarPago1SQL, insertarPago2SQL
+;
+
+            insertarPago1SQL = "SP_Pago1";
+
+            insertarPago2SQL = "SP_Pago2";
+
+            try
+            {
+                SqlCommand comando;
+                Pago c;
+                // GUARDANDO EL OBJETO Pago
+                if (Pago.couta != null)
+                {
+                    comando = gestorSQL.obtenerComandoSQL(insertarPago1SQL);
+                    comando.Parameters.AddWithValue("@coutaID", c.couta.CoutaID);
+                }
+                else
+                {
+                    comando = gestorSQL.obtenerComandoSQL(insertarPago2SQL);
+                }
+                comando.Parameters.AddWithValue("@idpago", c.PagoID);
+                comando.Parameters.AddWithValue("@diasmora", c.DiasMora);
+                comando.Parameters.AddWithValue("@montorecibido", c.MontoRecibido);
+                comando.Parameters.AddWithValue("@montototal", c.MontoTotal);
+                comando.Parameters.AddWithValue("@mora", c.Mora);
+                comando.Parameters.AddWithValue("@vuelto", c.Vuelto);
+                comando.ExecuteNonQuery();
+
+
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Ocurrio un problema al intentar guardar.", err);
+            }
+        }
+
+        public List<Pago> buscar(string pagoID)
+        {
+            List<Pago> listaDePagos = new List<Pago>();
+            Pago pago;
+            string consultaSQL = "select * from Pago where pagoID like '%" + pagoID + "%' order by pagoID";
+            try
+            {
+                SqlDataReader resultadoSQL = gestorSQL.ejecutarConsulta(consultaSQL);
+                while (resultadoSQL.Read())
+                {
+                    pago = obtenerPago(resultadoSQL);
+
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            return listaDePagos;
+        }
+
+        public Pago ConsultarPorfecha(string fecha)
+        {
+            Pago pago;
+            string consultaSQL = "select * from Couta where fecha = '" + fecha + "'";
+            try
+            {
+                SqlDataReader resultadoSQL = gestorSQL.ejecutarConsulta(consultaSQL);
+                if (resultadoSQL.Read())
+                {
+                    pago = obtenerPago(resultadoSQL);
+                }
+                else
+                {
+                    throw new Exception("No existe el pago.");
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            return pago;
+        }
+
+        private Pago obtenerPago(SqlDataReader resultadoSQL)
+        {
+            Pago pago = new Pago();
+            pago.PagoID = resultadoSQL.GetString(0);
+            pago.fecha = resultadoSQL.GetDateTime(0);
+            pago.DiasMora = resultadoSQL.GetInt32(1);
+            pago.MontoRecibido = Double.Parse(resultadoSQL.GetDecimal(2).ToString());
+            pago.MontoTotal = resultadoSQL.GetInt32(3);
+            pago.Mora = resultadoSQL.GetDouble(4);
+
+            return pago;
+        }
+
     }
 }
+    
+
+
+    
+
--- FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PrestamoDAO.cs
+++ FinancieraSoft.CapaPersistencia/FinancieraSoft.CapaPersistencia/SQLServerDAO/PrestamoDAO.cs
@@ -9,5 +9,132 @@ namespace FinancieraSoft.CapaPersistencia.SQLServerDAO
 {
     class PrestamoDAO
     {
+        private GestorSQL gestorSQL;
+
+        public PrestamoDAO(GestorSQL gestorSQL)
+        {
+            this.gestorSQL = gestorSQL;
+        }
+        public void generaPrestamo(Prestamo prestamo)
+        {
+            // CREANDO LAS SENTENCIAS SQL
+            string insertarPrestamo1SQL, insertarPrestamo2SQL, insertarCuotaSQL;
+
+            insertarPrestamo1SQL = "SP_GenerarPrestamo";
+
+            insertarPrestamo2SQL = "SP_GenerarPrestamo2";
+
+            insertarCuotaSQL = "SP_InsertarCuota";
+
+            try
+            {
+                SqlCommand comando;
+                Cuota cuota;
+                //GUARDANDO EL OBJETO Prestamo
+                if (prestamo.Cliente != null)
+                {
+                    comando = gestorSQL.obtenerComandoSQL(insertarPrestamo1SQL);
+                    comando.Parameters.AddWithValue("@codigoCliente", prestamo.Cliente.Codigo);
+                }
+                else
+                {
+                    comando = gestorSQL.obtenerComandoSQL(insertarPrestamo2SQL);
+                }
+                comando.Parameters.AddWithValue("@montoPrestado", prestamo.MontoPrestado);
+                comando.Parameters.AddWithValue("@tasaEfectivaAnual", prestamo.TasaEfectivaAnual);
+                comando.Parameters.AddWithValue("@totalPeriodosPago", prestamo.TotalPeriodosPago);
+                comando.Parameters.AddWithValue("@tasaEfectivaMensual", prestamo.TasaEfectivaMensual);
+                comando.Parameters.AddWithValue("@fechaPrestado", prestamo.FechaPrestamo);
+                comando.Parameters.AddWithValue("@cuotaFijaMensual", prestamo.TasaEfectivaAnual);
+                comando.ExecuteNonQuery();
+
+                // GUARDANDO LOS OBJETOS Lista de Cuotas
+                foreach (Cuota c in prestamo.ListaCuotas)
+                {
+                    cuota = c;
+                    // Agregando una couta del Prestamo
+                    comando = gestorSQL.obtenerComandoSQL(insertarCuotaSQL);
+                    comando.Parameters.AddWithValue("@prestamoID", prestamo.PrestamoID);
+                    comando.Parameters.AddWithValue("@saldo", cuota.Saldo);
+                    comando.Parameters.AddWithValue("@fecha", cuota.Fecha);
+                    comando.Parameters.AddWithValue("@amortizacion", cuota.Amortizacion);
+                    comando.Parameters.AddWithValue("@interes", cuota.Interes);
+                    comando.Parameters.AddWithValue("@fechaLimite", cuota.FechaLimite);
+                    comando.Parameters.AddWithValue("@estado", cuota.Estado);
+                    comando.ExecuteNonQuery();
+                }
+            }
+            catch (Exception err)
+            {
+                throw new Exception("Ocurrio un problema al intentar guardar.", err);
+            }
+
+
+        }
+
+        public List<Prestamo> buscar(string prestamoID)
+        {
+            List<Prestamo> listaDePrestamos = new List<Prestamo>();
+            Prestamo prestamo;
+            string consultaSQL = "select * from Prestamo where prestamoID like '%" + prestamoID + "%' order by prestamoID";
+            try
+            {
+                SqlDataReader resultadoSQL = gestorSQL.ejecutarConsulta(consultaSQL);
+                while (resultadoSQL.Read())
+                {
+                    prestamo = obtenerPrestamo(resultadoSQL);
+
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            return listaDePrestamos;
+        }
+
+        public Prestamo consultarPorDni(string Dni)
+        {
+            Prestamo prestamo;
+            string consultaSQL = "select * from Cliente where dni = '" + Dni + "'";
+            try
+            {
+                SqlDataReader resultadoSQL = gestorSQL.ejecutarConsulta(consultaSQL);
+                if (resultadoSQL.Read())
+                {
+                    prestamo = obtenerPrestamo(resultadoSQL);
+                }
+                else
+                {
+                    throw new Exception("No existe el cliente.");
+                }
+            }
+            catch (Exception err)
+            {
+                throw err;
+            }
+            return prestamo;
+        }
+
+        private Prestamo obtenerPrestamo(SqlDataReader resultadoSQL)
+        {
+            Prestamo prestamo = new Prestamo();
+            prestamo.PrestamoID = resultadoSQL.GetString(0);
+            prestamo.Dni = resultadoSQL.GetString(1);
+            prestamo.TotalPeriodosPago = resultadoSQL.GetInt32(2);
+            prestamo.FechaPrestamo = resultadoSQL.GetDateTime(3);
+            prestamo.MontoPrestado = resultadoSQL.GetFloat(4);
+            prestamo.TasaEfectivaAnual = Double.Parse(resultadoSQL.GetDecimal(5).ToString());
+
+
+            return prestamo;
+        }
+
     }
 }
+
+
+
+
+    
+
